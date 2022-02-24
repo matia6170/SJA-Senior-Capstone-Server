73e115677b0fedc2c7882a43fff16d34a483bbbf
matia6170 <matia6170@gmail.com>
2022-02-25 00:22:54 +0900

    removed ip

--- models/Survey.js
+++ models/Survey.js
@@ -3,10 +3,10 @@
 const mongoose = require("mongoose");
 
 const SurveySchema = new mongoose.Schema({
-  ip: {
+  /* ip: {
     type: String,
     required: true,
-  },
+  }, */
   name: {
     type: String,
     required: true,
